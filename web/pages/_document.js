1420c06f1bcf00c5fec5c97f452a287d5345a942
aXXo-dev <hunters12@protonmail.com>
2022-03-24 16:48:36 -0400

    Initial commit

---
+++ web/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/*Open Graph Tags*/}
+          <meta property="og:title" content="aXXo-dev" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://example.com/" />
+          <meta property="og:description" content="Welcome to my portfolio!" />
+          {/*Traditional Tags*/}
+          <meta title="aXXo-dev" />
+          <meta name="description" content="Welcome to my portfolio!" />
+          <meta name="theme-color" content="#fff8f3" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
